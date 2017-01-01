e56012d277463e4a180afb9b3269de14130eb59b
bvpk1989 <veerapavankumarbandi@gmail.com>
2017-01-01 12:53:18 +0530

    first commit

---
+++ src/main/java/com/opensource/FamilyTree1Application.java
@@ -0,0 +1,12 @@
+package com.opensource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyTree1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyTree1Application.class, args);
+	}
+}
---
+++ src/test/java/com/opensource/FamilyTree1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.opensource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FamilyTree1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
